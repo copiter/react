d7da7274e2def183c6a33980efd2e29bd65b0147
copiter <99804262+copiter@users.noreply.github.com>
2022-12-19 11:37:10 +0900

    .env 활용

--- 실전 리액트 프로그래밍/1.3_cra/src/App.js
+++ 실전 리액트 프로그래밍/1.3_cra/src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <TodoList />
+      {console.log(`NODE_ENV = ${process.env.NODE_ENV}`)}
+      {console.log(`NODE_ENV = ${process.env.REACT_APP_DATA_API}`)}
+      {console.log(`NODE_ENV = ${process.env.REACT_APP_LOGIN_API}`)}
     </div>
   );
 }
